d7ab5442ec8d990fd40acae8994bf1378794274a
Harsh9425 <harsh.web19@gmail.com>
2024-06-01 23:21:22 +0530

    MongoDB connected!

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,8 +1,15 @@
 import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 import { app } from "./app.js";
 
 dotenv.config();
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-});
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`⚙️  Server is running at port : ${process.env.PORT}`);
+        })
+    })
+    .catch((err) => {
+        console.log("MONGO db connection failed !!! ", err);
+    })
