dcdf2dda06f4cc1f403fbeeb9a1c1d218f2a7dd4
RealThomasCat <yashveer.rtc@gmail.com>
2024-06-21 23:25:23 +0530

    Frontend setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className=" text-xl font-bold">FileBud!</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ Frontend/src/main.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
+import ErrorPage from "./pages/ErrorPage.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
