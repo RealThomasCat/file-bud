c324976963aa5767ca42f0643738e5545b13d509
RealThomasCat <yashveer.rtc@gmail.com>
2024-07-09 22:46:13 +0530

    Implemented open folder and added image modal.

--- Frontend/src/components/FileCard.jsx
+++ Frontend/src/components/FileCard.jsx
@@ -4,50 +4,84 @@ import { OptionsButton } from "./index.js";
 import imageIcon from "../assets/ImageIcon.svg";
 import videoIcon from "../assets/VideoIcon.svg";
 import fileIcon from "../assets/FileIcon.svg";
-import fileService from "../services/file.service.js";
+import { Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
 
 function FileCard({ title = "File Name", type = "image", fileId }) {
-    const [thumbnailLink, setThumbnailLink] = useState("");
+    const [isOpen, setIsOpen] = useState(false);
 
     return (
-        // max-w-44 max-h-44
-        <div className="aspect-square text-textCol flex flex-col gap-3 bg-glass bg-opacity-10 border border-borderCol border-opacity-15 p-2 rounded-lg overflow-hidden">
-            <div className="w-full flex justify-between items-center px-1 py-0.5">
-                <div className="w-fit h-full flex items-center gap-2 overflow-hidden">
-                    <div className="h-4 w-4">
-                        <img
-                            className="h-full w-full object-contain"
-                            src={
-                                type === "image"
-                                    ? imageIcon
-                                    : type === "video"
-                                      ? videoIcon
-                                      : fileIcon
-                            }
-                            alt="File Type Icon"
-                        />
+        <>
+            <div
+                onDoubleClick={() => {
+                    if (type === "image") {
+                        setIsOpen(true);
+                    }
+                }}
+                className="aspect-square text-textCol flex flex-col gap-3 bg-glass bg-opacity-10 border border-borderCol border-opacity-15 p-2 rounded-lg overflow-hidden"
+            >
+                <div className="w-full flex justify-between items-center pl-1">
+                    <div className="w-fit h-full flex items-center gap-2 overflow-hidden">
+                        <div className="h-4 w-4">
+                            <img
+                                className="h-full w-full object-contain"
+                                src={
+                                    type === "image"
+                                        ? imageIcon
+                                        : type === "video"
+                                          ? videoIcon
+                                          : fileIcon
+                                }
+                                alt="File Type Icon"
+                            />
+                        </div>
+
+                        <h1 className="line-clamp-1 text-ellipsis">{title}</h1>
                     </div>
 
-                    <h1 className="line-clamp-1 text-ellipsis">{title}</h1>
+                    <OptionsButton />
                 </div>
 
-                <OptionsButton />
+                {/* Thumbnail */}
+                <div className="h-full w-full aspect-auto rounded-sm overflow-hidden">
+                    <img
+                        // src={thumbnailLink ? thumbnailLink : defaultThumbnail}
+                        src={
+                            type === "image" || type === "video"
+                                ? `http://localhost:8000/api/v1/files/thumbnail/${fileId}`
+                                : defaultThumbnail
+                        }
+                        alt="Khalistani Billa"
+                        className="w-full h-full object-cover rounded-sm"
+                    />
+                </div>
             </div>
 
-            {/* Thumbnail */}
-            <div className="h-full w-full aspect-auto rounded-sm overflow-hidden">
-                <img
-                    // src={thumbnailLink ? thumbnailLink : defaultThumbnail}
-                    src={
-                        type === "image" || type === "video"
-                            ? `http://localhost:8000/api/v1/files/thumbnail/${fileId}`
-                            : defaultThumbnail
-                    }
-                    alt="Khalistani Billa"
-                    className="w-full h-full object-cover rounded-sm"
-                />
-            </div>
-        </div>
+            <Dialog
+                open={isOpen}
+                onClose={() => setIsOpen(false)}
+                className="z-50"
+            >
+                <div className="fixed inset-0 flex flex-col w-screen items-center justify-center mx-auto bg-black bg-opacity-80">
+                    <DialogPanel className="w-fit h-full pt-16 mb-12 flex flex-col justify-center items-center">
+                        <div className="fixed max-h-16 min-h-16 h-16 max-w-7xl w-full p-2 top-0 text-xl">
+                            <div className="w-full h-full py-2 px-6 flex justify-between bg-glass bg-opacity-40 text-xl rounded-full text-textCol text-center border border-borderCol border-opacity-15">
+                                {/* TODO: OPTIONS */}
+                                <div>{title}</div>
+                                <div>Download</div>
+                            </div>
+                        </div>
+                        <div className="h-full">
+                            <img
+                                // TODO: Fetch file from backend
+                                src={`http://localhost:8000/api/v1/files/thumbnail/${fileId}`}
+                                alt={title}
+                                className="h-full object-contain"
+                            />
+                        </div>
+                    </DialogPanel>
+                </div>
+            </Dialog>
+        </>
     );
 }
 
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -17,7 +17,7 @@ function Home() {
     const [folder, setFolder] = useState(null);
     const [error, setError] = useState(null);
     const [files, setFiles] = useState([]);
-    const [subFolders, setSubFolders] = useState([]);
+    const [subfolders, setSubfolders] = useState([]);
 
     const authStatus = useSelector((state) => state.user.user);
     const rootFolderId = useSelector((state) => state.user.rootFolderId);
@@ -28,33 +28,10 @@ function Home() {
         const fetchFolder = async () => {
             try {
                 const response = await folderService.fetchFolder(rootFolderId);
+                console.log(response.data.data);
                 setFolder(response.data.data);
                 setFiles(response.data.data.files);
-                setSubFolders(response.data.data.subFolders);
-
-                // Fetch thumbnails for each file
-                // const filesWithThumbnails = await Promise.all(
-                //     response.data.data.files.map(async (file) => {
-                //         try {
-                //             const thumbnailResponse =
-                //                 await fileService.fetchThumbnail(file._id);
-
-                //             console.log(thumbnailResponse);
-
-                //             const thumbnailUrl = URL.createObjectURL(
-                //                 thumbnailResponse.data
-                //             );
-                //             return { ...file, thumbnailUrl };
-                //         } catch (error) {
-                //             console.error(
-                //                 `Error fetching thumbnail for file ${file._id}:`,
-                //                 error
-                //             );
-                //             return { ...file, thumbnailUrl: null }; // Handle error by setting thumbnailUrl to null
-                //         }
-                //     })
-                // );
-                // setFiles(filesWithThumbnails);
+                setSubfolders(response.data.data.subfolders);
             } catch (error) {
                 console.error(error.message);
                 setError(error.message);
@@ -106,16 +83,16 @@ function Home() {
             </div>
 
             {(files && files.length > 0) ||
-            (subFolders && subFolders.length > 0) ? (
-                <div className="flex flex-col">
+            (subfolders && subfolders.length > 0) ? (
+                <div className="flex flex-col gap-2">
                     {/* Folders */}
-                    {subFolders && subFolders.length > 0 && (
+                    {subfolders && subfolders.length > 0 && (
                         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 py-4">
-                            {subFolders.map((subFolder) => (
-                                <div key={subFolder._id}>
-                                    <Link to={`/folders/${subFolder._id}`}>
+                            {subfolders.map((subfolder) => (
+                                <div key={subfolder._id}>
+                                    <Link to={`/folders/${subfolder._id}`}>
                                         <FolderCard
-                                            title={subFolder.title} // TODO: show file details after populating the file object
+                                            title={subfolder.title} // TODO: show file details after populating the file object
                                         />
                                     </Link>
                                 </div>
