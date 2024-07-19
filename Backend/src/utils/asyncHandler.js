5ad75d6f7fb0dcb3cd576703a21aef242a32480d
Harsh9425 <harsh.web19@gmail.com>
2024-07-20 00:33:43 +0530

    Update asyncHandler.js for better Error messages

--- Backend/src/utils/asyncHandler.js
+++ Backend/src/utils/asyncHandler.js
@@ -10,9 +10,13 @@ const asyncHandler = (requestHandler) => {
         try {
             await requestHandler(req, res, next);
         } catch (err) {
+            // console.log(err);
             if (err instanceof ApiError) {
                 res.status(err.statusCode).json({
                     message: err.message,
+                    statusCode: err.statusCode,
+                    success: err.success,
+                    errors: err.errors,
                 });
             } else {
                 res.status(500).json({
